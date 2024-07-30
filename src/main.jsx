056024f5c424af034e7f94ec3361bd62484a2c3a
Al Amin <alaminbdt50@gmail.com>
2024-07-31 01:49:08 +0600

    react-router-dom added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import Navbar from './Navbar.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Navbar></Navbar>
+       <RouterProvider router={router} />
   </React.StrictMode>,
 )
