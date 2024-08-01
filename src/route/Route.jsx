a988889b86a2b66d929d30deab9a12160f287b61
roy-biplobe <biplobkumargk@gmail.com>
2024-08-01 13:16:25 +0600

    admin pannel route setup done

--- src/route/Route.jsx
+++ src/route/Route.jsx
@@ -1,10 +1,15 @@
 import {
   createBrowserRouter,
 } from "react-router-dom";
+import Dashboard from "../pages/adminSide/Dashboard";
 
  export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/home ",
     element: <div>Hello world!</div>,
   },
+  {
+    path:"/admin-pannel",
+    element:<Dashboard></Dashboard>
+  }
 ]);
