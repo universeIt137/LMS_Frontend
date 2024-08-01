cbc05c47427d031ded44c570871461fe9592055f
roy-biplobe <biplobkumargk@gmail.com>
2024-08-01 12:56:59 +0600

    route folder setup and route separate

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from 'react-router-dom'
+import { router } from './route/Route.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
        <RouterProvider router={router} />
