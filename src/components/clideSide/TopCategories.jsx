825aca14942441f9862d436da0b82ffe8c1cef31
Al Amin <alaminbdt50@gmail.com>
2024-08-10 00:34:26 +0600

    top-categori added

---
+++ src/components/clideSide/TopCategories.jsx
@@ -0,0 +1,23 @@
+import TopCategori from "./TopCategori";
+
+
+const TopCategories = () => {
+    return (
+        <div className="grid grid-cols-3 gap-4">
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+            <TopCategori></TopCategori>
+        </div>
+    );
+};
+
+export default TopCategories;
