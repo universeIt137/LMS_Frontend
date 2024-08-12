9d3f3dea95b9da977cde1deb0adc1c843b92d782
roy-biplobe <biplobkumargk@gmail.com>
2024-08-12 18:42:49 +0600

    favicon change

--- src/components/clideSide/ClientFooter.jsx
+++ src/components/clideSide/ClientFooter.jsx
@@ -1,46 +1,47 @@
+import { Link } from "react-router-dom";
 
 
 const ClientFooter = () => {
     return (
         <div>
             <footer className="footer bg-[#171719]  text-white p-10">
-                <aside>
-                    <svg
-                        width="50"
-                        height="50"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        className="fill-current">
-                        <path
-                            d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-                    </svg>
+                <aside className="space-y-4">
+                    <Link to="/"><img className="w-[150px]" src="https://demo.creativeitem.com/academy/uploads/system/b75d56b5a93590ce01464acd96a853f1.png" alt="" /></Link>
                     <p>
-                        ACME Industries Ltd.
-                        <br />
-                        Providing reliable tech since 1992
+                        Study any topic, anytime. explore thousands of courses for the lowest price ever!
+
+
                     </p>
+                    <p className="text-xl"> Subscribe to our newsletter</p>
+                    <div className="join">
+                        <input className="input input-bordered text-black  join-item" placeholder="Email" />
+                        <button className="btn join-item ">Subscribe</button>
+                    </div>
                 </aside>
                 <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <h6 className="footer-title text-white">Top categories</h6>
+                    <Link to="/html-css" className="link link-hover">HTML & CSS</Link>
+                    <Link to="/color-theory" className="link link-hover">Color Theory</Link>
+                    <Link to="/photoshop" className="link link-hover">Photoshop</Link>
+                    <Link to="/wordpress-theme" className="link link-hover">WordPress Theme</Link>
+                    <Link to="/adobe-illustrator" className="link link-hover">Adobe Illustrator</Link>
+                    <Link to="/bootstrap" className="link link-hover">Bootstrap</Link>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <h6 className="footer-title">Useful links</h6>
+                    <Link to="/become-instructor" className="link link-hover">Become an instructor</Link>
+                    <Link to="/blog" className="link link-hover">Blog</Link>
+                    <Link to="/all-courses" className="link link-hover">All courses</Link>
+                    <Link to="/sign-up" className="link link-hover">Sign up</Link>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <h6 className="footer-title">Help</h6>
+                    <Link to="/contact-us" className="link link-hover">Contact us</Link>
+                    <Link to="/about-us" className="link link-hover">About us</Link>
+                    <Link to="/privacy-policy" className="link link-hover">Privacy policy</Link>
+                    <Link to="/terms-and-conditions" className="link link-hover">Terms and conditions</Link>
+                    <Link to="/faq" className="link link-hover">Faq</Link>
+                    <Link to="/refund-policy" className="link link-hover">Refund policy</Link>
                 </nav>
             </footer>
 
