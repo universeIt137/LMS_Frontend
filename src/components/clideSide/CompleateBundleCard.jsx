0bd27305cf2a9413e9c5b9b458a02ba0c83b0d71
Al Amin <alaminbdt50@gmail.com>
2024-08-13 17:38:54 +0600

    responsive course bundle

--- src/components/clideSide/CompleateBundleCard.jsx
+++ src/components/clideSide/CompleateBundleCard.jsx
@@ -2,17 +2,17 @@ import bundleThumbnail from "../../assets/image/course_thumbnail.jpg";
 
 const CompleateBundleCard = () => {
     return (
-        <div className="card shadow-sm bg-gray-100  h-96 p-6 overflow-hidden">
-            <div className="flex justify-between text-black mb-4">
+        <div className="card shadow-[0_4px_6px_rgba(255,255,255,0.5)] bg-gray-100 h-96 p-6 overflow-hidden">
+            <div className="flex flex-col md:flex-row justify-between text-black mb-4">
                 <h1 className="text-xl font-bold">Complete Solution</h1>
-                <div className="flex">
+                <div className="flex md:flex-row flex-col md:items-center md:ml-4 mt-2 md:mt-0">
                     <h1 className="text-xl font-bold">Course 4</h1>
-                    <p className="ml-4 text-xl font-bold">$74</p>
+                    <p className="ml-0 md:ml-4 text-xl font-bold">$74</p>
                 </div>
             </div>
 
             <div className="h-64 overflow-y-scroll">
-                <div className="max-w-full p-4 bg-white rounded-lg shadow-md mb-4">
+                <div className="max-w-full p-4 bg-white rounded-lg shadow-[0_4px_6px_rgba(255,255,255,0.5)] mb-4">
                     <div className="flex px-4">
                         <img src={bundleThumbnail} className="mb-2 w-16" alt="Course Thumbnail" />
                         <div className="ml-4">
@@ -21,7 +21,7 @@ const CompleateBundleCard = () => {
                     </div>
                 </div>
 
-                <div className="max-w-full p-4 bg-white rounded-lg shadow-md mb-4">
+                <div className="max-w-full p-4 bg-white rounded-lg shadow-[0_4px_6px_rgba(255,255,255,0.5)] mb-4">
                     <div className="flex px-4">
                         <img src={bundleThumbnail} className="mb-2 w-16" alt="Course Thumbnail" />
                         <div className="ml-4">
@@ -30,7 +30,7 @@ const CompleateBundleCard = () => {
                     </div>
                 </div>
 
-                <div className="max-w-full p-4 bg-white rounded-lg shadow-md mb-4">
+                <div className="max-w-full p-4 bg-white rounded-lg shadow-[0_4px_6px_rgba(255,255,255,0.5)] mb-4">
                     <div className="flex px-4">
                         <img src={bundleThumbnail} className="mb-2 w-16" alt="Course Thumbnail" />
                         <div className="ml-4">
@@ -39,7 +39,7 @@ const CompleateBundleCard = () => {
                     </div>
                 </div>
 
-                <div className="max-w-full p-4 bg-white rounded-lg shadow-md mb-4">
+                <div className="max-w-full p-4 bg-white rounded-lg shadow-[0_4px_6px_rgba(255,255,255,0.5)] mb-4">
                     <div className="flex px-4">
                         <img src={bundleThumbnail} className="mb-2 w-16" alt="Course Thumbnail" />
                         <div className="ml-4">
@@ -48,9 +48,9 @@ const CompleateBundleCard = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex justify-between">
-                <button className="btn btn-primary w-1/2 mr-2">Bundle Details</button>
-                <button className="btn btn-primary w-1/2 ml-2">$70 buy now</button>
+            <div className="flex justify-between mt-4">
+                <button className="btn bg-[#754FFE] w-1/2 mr-2">Bundle Details</button>
+                <button className="btn bg-[#754FFE] w-1/2 ml-2">$70 buy now</button>
             </div>
         </div>
     );
