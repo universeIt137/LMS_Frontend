7d7c3a24c1762f26f06545f92ae74967e7e8b707
roy-biplobe <biplobkumargk@gmail.com>
2024-08-13 10:47:27 +0600

    Think more clearly responsive done

--- src/components/clideSide/ThinkMoreClearlyCard.jsx
+++ src/components/clideSide/ThinkMoreClearlyCard.jsx
@@ -2,15 +2,15 @@ import PropTypes from 'prop-types';
 const ThinkMoreClearlyCard = ({title, discription, img}) => {
   return (
     <div>
-      <div className="card card-side bg-base-50 shadow-md p-10">
+      <div className="card flex-col-reverse card-side bg-base-50 shadow-md p-10">
 
-        <div className="card-body w-3/4 ">
+        <div className="card-body w-full md:w-3/4 ">
           <h2 className="text-4xl mb-6 ">
             {title}</h2>
           <p className="text-2xl text-[#676c7d]">{discription}</p>
           
         </div>
-        <figure  className=" w-1/4 ">
+        <figure  className="w-full md:w-1/4 ">
           <img  className="rounded-2xl"
             src={img}
             alt="Movie" />
