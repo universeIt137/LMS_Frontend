c1a95e76afeb35a132d67fe9fb616aba350eb284
ashikur <ashikur1602@gmail.com>
2024-09-05 11:35:26 +0600

    Dashboard layout completed

--- src/components/adminside/Sidebar.jsx
+++ src/components/adminside/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { BsMenuButtonWideFill } from 'react-icons/bs';
 import { GrMoney } from 'react-icons/gr';
-import { IoMdCard } from 'react-icons/io';
+import { IoIosAddCircleOutline, IoMdCard } from 'react-icons/io';
 import { IoCartOutline, IoCloseCircleOutline } from 'react-icons/io5';
-import { MdMenuOpen, MdOutlineAdminPanelSettings, MdOutlineCategory, MdOutlineDiscount, MdOutlineLockPerson, MdOutlinePeopleOutline } from 'react-icons/md';
+import { MdAddCircleOutline, MdMenuOpen, MdOutlineAdminPanelSettings, MdOutlineCategory, MdOutlineDiscount, MdOutlineLockPerson, MdOutlineManageSearch, MdOutlinePeopleOutline } from 'react-icons/md';
 import { RiAdminLine } from 'react-icons/ri';
 import { RxDashboard } from 'react-icons/rx';
 import { NavLink } from 'react-router-dom';
@@ -19,7 +19,7 @@ const Sidebar = () => {
     return (
         <>
             <aside
-                className={`bg-dasboard_theme text-white transition-all duration-300 ${isSidebarOpen ? 'w-64' : 'w-14'
+                className={`bg-dashboard_theme text-white transition-all duration-300 ${isSidebarOpen ? 'w-64' : 'w-14'
                     }`}
             >
                 <div className="flex justify-between items-center p-4 gap-8">
@@ -39,84 +39,24 @@ const Sidebar = () => {
                             <NavLink
                                 exact
                                 to="/dashboard"
-                                className="flex items-center space-x-3 p-3 rounded-lg transition duration-200 hover:bg-indigo-700 hover:shadow-md hover:border-b"
+                                className="flex items-center space-x-3 p-3 rounded-lg transition duration-200  hover:shadow-md hover:border-b hover:text-dashboard_hover_text"
                                 activeClassName="bg-indigo-900 shadow-lg"
                             >
-                                <RxDashboard className='text-3xl' />
+                                <MdAddCircleOutline className='font-bold text-xl' />
                                 <span className={`${isSidebarOpen ? 'block' : 'hidden'}`}>
-                                    <p className='font-bold text-xl'>Dashboard</p>
+                                    <p className='font-bold'>Add Course</p>
                                 </span>
                             </NavLink>
                         </li>
                         <li>
                             <NavLink
                                 to="/order-management"
-                                className="flex items-center space-x-3 p-3 rounded-lg transition duration-200 hover:bg-indigo-700 hover:shadow-md hover:border-b"
+                                className="flex items-center space-x-3 p-3 rounded-lg transition duration-200  hover:shadow-md hover:border-b"
                                 activeClassName="bg-indigo-900 shadow-lg"
                             >
-                                <IoCartOutline className='text-4xl' />
+                                <MdOutlineManageSearch className='font-bold text-xl' />
                                 <span className={`${isSidebarOpen ? 'block' : 'hidden'}`}>
-                                    <p className='text-[18px] font-bold'>Order Management</p>
-                                </span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to="/coupon-code"
-                                className="flex items-center space-x-3 p-3 rounded-lg transition duration-200 hover:bg-indigo-700 hover:shadow-md hover:border-b"
-                                activeClassName="bg-indigo-900 shadow-lg"
-                            >
-                                <MdOutlineDiscount className='text-3xl' />
-                                <span className={`${isSidebarOpen ? 'block' : 'hidden'}`}>
-                                    <p className='text-[18px] font-bold'>Coupon Code</p>
-                                </span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to="/categories"
-                                className="flex items-center space-x-3 p-3 rounded-lg transition duration-200 hover:bg-indigo-700 hover:shadow-md hover:border-b"
-                                activeClassName="bg-indigo-900 shadow-lg"
-                            >
-                                <MdOutlineCategory className='text-4xl' />
-                                <span className={`${isSidebarOpen ? 'block' : 'hidden'}`}>
-                                    <p className='text-[18px] font-bold'>Categories</p>
-                                </span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to="/transaction"
-                                className="flex items-center space-x-3 p-3 rounded-lg transition duration-200 hover:bg-indigo-700 hover:shadow-md hover:border-b"
-                                activeClassName="bg-indigo-900 shadow-lg"
-                            >
-                                <IoMdCard className='text-3xl' />
-                                <span className={`${isSidebarOpen ? 'block' : 'hidden'}`}>
-                                    <p className='text-[18px] font-bold'>Transactions</p>
-                                </span>
-                            </NavLink>
-                        </li>
-                        <li className="mt-10">
-                            <NavLink
-                                to="/manage-admins"
-                                className="flex items-center space-x-3 p-3 rounded-lg transition duration-200 hover:bg-indigo-700 hover:shadow-md hover:border-b"
-                                activeClassName="bg-indigo-900 shadow-lg"
-                            >
-                                <MdOutlineAdminPanelSettings className='text-3xl' />
-                                <span className={`${isSidebarOpen ? 'block' : 'hidden'}`}>
-                                    <p className="text-[18px] font-bold">Manage Admins</p>
-                                </span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                to="/admin-roles"
-                                className="flex items-center space-x-3 p-3 rounded-lg transition duration-200 hover:bg-indigo-700 hover:shadow-md hover:border-b"
-                                activeClassName="bg-indigo-900 shadow-lg"
-                            >
-                                <MdOutlinePeopleOutline className='text-3xl' />
-                                <span className={`${isSidebarOpen ? 'block' : 'hidden'}`}>
-                                    <p className="text-[18px] font-bold">Admin Roles</p>
+                                    <p className='font-bold'>Manage Courses</p>
                                 </span>
                             </NavLink>
                         </li>
